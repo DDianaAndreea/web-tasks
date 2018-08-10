deeb81785222470255c88e3a5ce2597c294d5f2b
DDianaAndreea <33541782+DDianaAndreea@users.noreply.github.com>
2018-08-10 16:39:10 +0300

    tentative

---
+++ src/components/Users.js
@@ -0,0 +1,42 @@
+import React, {Component, Fragment} from 'react';
+import axios from 'axios';
+import {Link} from "react-router-dom";
+
+export default class Users extends Component {
+    state = {
+        users: []
+    };
+
+    async componentDidMount() {
+        let users = await axios.get('http://api.local/v1/users')
+        .then(res=>{
+            this.setState({users: users.data});
+        })
+        
+    }
+
+    _logout = () => {
+        sessionStorage.removeItem('token');
+
+        this.props.history.push('/');
+    };
+
+    render() {
+        // if (!sessionStorage.getItem('token')) {
+        //     return <Redirect to={'/login'}/>
+        // }
+
+        // const {users} = this.state;
+
+        return (
+            <Fragment>
+                {/* {users.map((user, key) => <p key={key}>{user.name}</p>)} */}
+                <ul>
+                    { this.state.users.map(user => <li>{user.name}</li>)}
+                </ul>
+                <p>Return <Link to={'/'}>Home</Link>.</p>
+                <button onClick={this._logout}>Logout</button>
+            </Fragment>
+        )
+    }
+}
